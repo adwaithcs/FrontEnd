0841c70f350d25b1f98a24074772076944cb182a
adwaithcs <129148946+adwaithcs@users.noreply.github.com>
2023-05-18 12:14:32 +0530

    Add files via upload

--- adw/m12(1).py
+++ adw/m12(1).py
@@ -1,9 +1,8 @@
 # Write a program that takes a list of integers as input and outputs the largest integer in the list using a while loop
 
-list=[99,12,15,20,17]
-i=0
-j=0
-# a=list[0]
+# list=[99,12,15,20,17]
+# i=0
+# j=0
 # b=len(list)
 # while i<b:
 #     if list[i]>list[i+1]:
@@ -11,7 +10,7 @@ j=0
 #         j=j+1
 #         j+=1    
 #     i=i+1
-numbers = [100,1,2,5,8,4,99,3]
+numbers = [10,1,2,5,8,4,99,3]
 
 x = 0
 y = numbers[x]
