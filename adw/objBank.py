c07d815b087e4ade136d5b79a8763d0c3dabad63
adwaithcs <129148946+adwaithcs@users.noreply.github.com>
2023-05-31 13:08:32 +0530

    Add files via upload

---
+++ adw/objBank.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self,acno,balance):
+        self.no=acno
+        self.bal=balance
+        
+    def Deposit(self,acno,balance):
+        print('AcNo:',x)
+        print('Balance:',y)
+        
+        
+            
+x=int(input("Enter your account number:"))
+y=0
+# dp=int(input('Enter the amount:'))
+
+
+obj=BankAccount(x,y)
+obj.Deposit(x,y)
+            
